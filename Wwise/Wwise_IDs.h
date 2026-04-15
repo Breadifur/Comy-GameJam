a169bccc2c8581e0b6a27e0a123c6740928dfa25
Breadifur <bjpainter301@gmail.com>
2026-04-15 15:32:49 -0400

    InitWwise file
    meh

---
+++ Wwise/Wwise_IDs.h
@@ -0,0 +1,138 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID NEW_EVENT = 3050945240U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace COMBATSTATE
+        {
+            static const AkUniqueID GROUP = 1071758680U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace COMBATSTATE
+
+        namespace MAINMENUSTATE
+        {
+            static const AkUniqueID GROUP = 4015577990U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MUSICON = 3998479257U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MAINMENUSTATE
+
+        namespace TALKINGSTATE
+        {
+            static const AkUniqueID GROUP = 4107386924U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace TALKINGSTATE
+
+        namespace WORLDSTATE
+        {
+            static const AkUniqueID GROUP = 1048369770U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MUSICON = 3998479257U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace WORLDSTATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTJUMPNPC
+        {
+            static const AkUniqueID GROUP = 660707862U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRTJUMP = 1835284706U;
+                static const AkUniqueID GRASSJUMP = 2628072365U;
+                static const AkUniqueID GRAVELJUMP = 1128852020U;
+                static const AkUniqueID STONEJUMP = 1608852672U;
+                static const AkUniqueID WOODJUMP = 2564151170U;
+            } // namespace SWITCH
+        } // namespace FOOTJUMPNPC
+
+        namespace FOOTJUMPPC
+        {
+            static const AkUniqueID GROUP = 57372634U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRTJUMP = 1835284706U;
+                static const AkUniqueID GRASSJUMP = 2628072365U;
+                static const AkUniqueID GRAVELJUMP = 1128852020U;
+                static const AkUniqueID STONEJUMP = 1608852672U;
+                static const AkUniqueID WOODJUMP = 2564151170U;
+            } // namespace SWITCH
+        } // namespace FOOTJUMPPC
+
+        namespace FOOTSTEPSNPC
+        {
+            static const AkUniqueID GROUP = 1682058159U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPSNPC
+
+        namespace FOOTSTEPSPC
+        {
+            static const AkUniqueID GROUP = 436374629U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPSPC
+
+    } // namespace SWITCHES
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ Wwise/resources/wwise_ids.gd
@@ -0,0 +1,106 @@
+class_name AK
+
+class EVENTS:
+
+	const NEW_EVENT : int = 3050945240
+
+class STATES:
+
+	class COMBATSTATE:
+		const GROUP : int = 1071758680
+	
+		class STATE:
+			const NONE : int = 748895195
+
+	class MAINMENUSTATE:
+		const GROUP : int = 4015577990
+	
+		class STATE:
+			const MUSICON : int = 3998479257
+			const NONE : int = 748895195
+
+	class TALKINGSTATE:
+		const GROUP : int = 4107386924
+	
+		class STATE:
+			const NONE : int = 748895195
+
+	class WORLDSTATE:
+		const GROUP : int = 1048369770
+	
+		class STATE:
+			const MUSICON : int = 3998479257
+			const NONE : int = 748895195
+
+
+class SWITCHES:
+
+	class FOOTJUMPNPC:
+		const GROUP : int = 660707862
+	
+		class SWITCH:
+			const DIRTJUMP : int = 1835284706
+			const GRASSJUMP : int = 2628072365
+			const GRAVELJUMP : int = 1128852020
+			const STONEJUMP : int = 1608852672
+			const WOODJUMP : int = 2564151170
+
+	class FOOTSTEPSNPC:
+		const GROUP : int = 1682058159
+	
+		class SWITCH:
+			const DIRT : int = 2195636714
+			const GRASS : int = 4248645337
+			const GRAVEL : int = 2185786256
+			const STONE : int = 1216965916
+			const WOOD : int = 2058049674
+
+	class FOOTJUMPPC:
+		const GROUP : int = 57372634
+	
+		class SWITCH:
+			const DIRTJUMP : int = 1835284706
+			const GRASSJUMP : int = 2628072365
+			const GRAVELJUMP : int = 1128852020
+			const STONEJUMP : int = 1608852672
+			const WOODJUMP : int = 2564151170
+
+	class FOOTSTEPSPC:
+		const GROUP : int = 436374629
+	
+		class SWITCH:
+			const DIRT : int = 2195636714
+			const GRASS : int = 4248645337
+			const GRAVEL : int = 2185786256
+			const STONE : int = 1216965916
+			const WOOD : int = 2058049674
+
+
+class GAME_PARAMETERS:
+	pass
+
+class TRIGGERS:
+	pass
+
+class BANKS:
+	pass
+
+class AUX_BUSSES:
+	pass
+
+class ACOUSTIC_TEXTURES:
+
+	const ACOUSTIC_BANNER : int = 4168643977
+	const ANECHOIC : int = 1873957695
+	const BRICK : int = 504532776
+	const CARPET : int = 2412606308
+	const CONCRETE : int = 841620460
+	const CORK_TILES : int = 3195498748
+	const CURTAINS : int = 2928161104
+	const DRYWALL : int = 3670307564
+	const FABRIC : int = 1970351858
+	const MOUNTAIN : int = 513139656
+	const TILE : int = 2637588553
+	const WOOD : int = 2058049674
+	const WOOD_BRIGHT : int = 4262522749
+	const WOOD_DEEP : int = 1755085759
